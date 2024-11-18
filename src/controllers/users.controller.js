37b61312eb746a96e5c24bee068107c0eb871ce3
Daniel Iovescu <iovescu.dani@gmail.com>
2024-11-18 19:45:52 +0200

    Added CRUD logic for users

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,16 +1,37 @@
 class UsersController {
+  // {id: 1, name: 'User A', email: 'test@test.com'}
+  /**
+   * [
+   * {id: 1, name: 'User A', email: 'test@test.com'},
+   * {id: 2, name: 'User B', email: 'test1@test.com'},
+   * {id: 3, name: 'User C', email: 'test2@test.com'},
+   * {id: 4, name: 'User D', email: 'test3@test.com'},
+   * {id: 5, name: 'User E', email: 'test4@test.com'},
+   * {id: 6, name: 'User E', email: 'test4@test.com'}
+   * ]
+   *  */
+  users = [];
 
-    getAll() {
-        // SELECT * FROM users.....
-    }
+  getAll() {
+    return this.users;
+  }
 
-    get(id) {
-        // SELECT * FROM users WHERE id = id
-    }
+  get(id) {
+    return this.users.find(function (user) {
+      return user.id === id;
+    });
+  }
 
-    create(user) {
-        // INSERT INTO users VALUES ......
-    }
+  create(user) {
+    this.users.push(user);
+  }
+
+  delete(id) { // 3
+    const index = this.users.findIndex(function(user) {
+        return user.id === id;
+    });
+    this.users.splice(index, 1);
+  }
 }
 
 module.exports = UsersController;
