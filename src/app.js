f13b1be009752ff4ea6a6760f7b6972de0b58f0a
Daniel Iovescu <iovescu.dani@gmail.com>
2024-11-18 19:17:20 +0200

    Added start script and defiend constants

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const constants = require('./utils/constants');
+
 // Routes imports
 const UsersRoute = require("./routes/users.route");
 
@@ -10,7 +12,6 @@ app.use(bodyParser.json());
 
 new UsersRoute(app);
 
-const PORT = 3000;
-app.listen(PORT, function () {
-  console.log("Server is running on port: " + PORT);
+app.listen(constants.PORT, function () {
+  console.log("Server is running on port: " + constants.PORT);
 });
