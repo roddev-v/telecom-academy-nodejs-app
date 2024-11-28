e5623996cac3e0959f6275325a30c60f13c392ad
Daniel Iovescu <iovescu.dani@gmail.com>
2024-11-28 20:23:52 +0200

    added firebase admin

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,14 @@ import express from "express";
 import * as bodyParser from "body-parser";
 import { PORT } from "./utils/constants";
 import { UserRoute } from "./routes/users.route";
+import * as firebaseConfig from './db/firebase-config.json';
+import * as admin from 'firebase-admin'
 
 const app = express();
+
+admin.initializeApp({
+  credential: admin.credential.cert(firebaseConfig as admin.ServiceAccount)
+});
 
 app.use(bodyParser.json());
 
