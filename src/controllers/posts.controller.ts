ae6379520a43d4163aa00ece242ef8dfb4613b54
Daniel Iovescu <iovescu.dani@gmail.com>
2024-11-28 21:13:03 +0200

    added function to delete

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -8,10 +8,24 @@ type Post = {
 class PostsController {
   db = admin.firestore();
 
-  getAll() {}
+  async getAll() {
+    const res = await this.db.collection("posts").get();
+    return res.docs.map((item) => {
+      return item.data();
+    });
+  }
+
+  async get(id: string) {
+    const response = await this.db.collection("posts").doc(id).get();
+    return response.data();
+  }
 
   async create(post: Post) {
     await this.db.collection("posts").add(post);
+  }
+
+  async delete(id: string) {
+    await this.db.collection("posts").doc(id).delete();
   }
 }
 
--- src/routes/posts.route.ts
+++ src/routes/posts.route.ts
@@ -10,12 +10,31 @@ export class PostsRoute {
   }
 
   initRoutes(app: express.Express): void {
-    app.get("/posts", (req, res) => {});
+    app.get("/posts", async (req, res) => {
+      const posts = await this.postsController.getAll();
+      res.json(posts);
+    });
+
+    app.get("/posts/:id", async (req, res) => {
+      const userId = req.params.id;
+      const user = await this.postsController.get(userId);
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).json({ message: "Post not found" });
+      }
+    });
 
     app.post("/posts", async (req, res) => {
       const post = req.body;
       await this.postsController.create(post);
       res.json(post);
     });
+
+    app.delete("/users/:id", async (req, res) => {
+        const postId = req.params.id;
+        await this.postsController.delete(postId);
+        res.send({ id: postId });
+      });
   }
 }
